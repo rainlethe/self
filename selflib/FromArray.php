c352ad6e3ea0bfbc0693e5bda761b11037c06a34
rainlethe <rainlethez@gmail.com>
2015-12-08 18:10:08 +0900

    add array to diff

--- selflib/FromArray.php
+++ selflib/FromArray.php
@@ -16,6 +16,58 @@ class FromArray{
 		return $this;
 	}
 	
+
+	/* array_column */
+	public function column($column_key, $index_key = null){
+		$this->__invar = array_column($this->__invar, $column_key, $index_key);
+		return $this;
+	}
+
+	/* array_combine */
+	public function combineKeys($values){
+		$this->__invar = array_combine($this->__invar, $values);
+		return $this;	
+	}
+
+	/* array_combine */
+	public function combineValues($keys){
+		$this->__invar = array_combine($values, $this->__invar);
+		return $this;	
+	}
+
+	/* array_count_values */
+	public function countValues(){
+		$this->__invar = array_count_values($this->__invar);
+		return $this;
+	}
+
+	/* array_diff_key , array_diff_ukey */
+	public function diffKey($diffarray, $funcName=null){		
+		if ($funcName == null){
+			$this->__invar = array_diff_key($this->__invar, $diffarray);
+		}else{
+			$this->__invar = array_diff_ukey($this->__invar, $diffarray, $funcName);
+		}
+		
+		return $this;
+	}
+
+	/* array_diff_assoc, array_diff_uassoc */
+	public function diffKeyValue($diffarray, $funcName=null){
+		if ($funcName == null){
+			$this->__invar = array_diff_assoc($this->__invar, $diffarray);
+		}else{
+			$this->__invar = array_diff_uassoc($this->__invar, $diffarray, $funcName);
+		}
+		return $this;
+	}
+
+	/* array_diff */
+	public function diffValue($diffArray){
+		$this->__invar = array_diff_key($this->__invar, $diffArray);
+	}
+
+	/* array_chunk */
 	public function chunk($size, $holdkey=false){
 		$this->__invar = array_chunk($this->__invar, $size, $holdkey);
 		return $this;
--- test.php
+++ test.php
@@ -29,7 +29,18 @@ include_once('self.php');
 
 // $self->from("a나bc가 d")->reverse()->out();
 
-$arr = array(1,2,3,4,5);
+// $arr = array(1,2,3,4,5);
 
+function key_compare_func($a, $b)
+{	
+    if ($a === $b) {
+        return 0;
+    }
+    return ($a > $b)? 1:-1;
+}
 
+$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
+$array2 = array("a" => "green", "yellow", "red");
+$result = array_diff_uassoc($array1, $array2, "key_compare_func");
+print_r($result);
 ?>
